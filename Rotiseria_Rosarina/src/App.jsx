7df86d89b1c33dec4f01b3fce96f7891292f09a9
JuanFuentes1 <juanfuentes3168a@gmail.com>
2024-05-07 22:03:09 -0300

    inicio del proyecto

---
+++ Rotiseria_Rosarina/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
